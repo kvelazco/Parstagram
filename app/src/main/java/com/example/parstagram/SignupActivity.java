ea71a2f6c5687d8e188fc6060b6aff9cf29eafaf
kvelazco <kvela033@fiu.edu>
2021-10-03 22:39:25 -0400

    missing files added

---
+++ app/src/main/java/com/example/parstagram/SignupActivity.java
@@ -0,0 +1,88 @@
+package com.example.parstagram;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignupActivity extends AppCompatActivity {
+
+    public static final String TAG = "SignupActivity";
+    private EditText etUsernameSignup;
+    private EditText etEmailSignup;
+    private EditText etPasswordSignup;
+    private Button btnCreateAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.nav_logo_whiteout);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+
+        etUsernameSignup = findViewById(R.id.etUsernameSignup);
+        etEmailSignup = findViewById(R.id.etEmailSignup);
+        etPasswordSignup = findViewById(R.id.etPasswordSignUp);
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick create account button");
+                String username = etUsernameSignup.getText().toString();
+                String email = etEmailSignup.getText().toString();
+                String password = etPasswordSignup.getText().toString();
+                createUser(username, email, password);
+            }
+        });
+    }
+
+    private void createUser(String username, String email, String password) {
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        // Set custom properties
+        //user.put("phone", "650-253-0000");
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                    goMainActivity();
+                    Toast.makeText(SignupActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                    Log.e(TAG, "Issue with account creation", e);
+                    //return;
+                }
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+}
+
+
+
